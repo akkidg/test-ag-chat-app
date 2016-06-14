33d63123cf1dbf5ad3bba6c9fa29dfda1ee0dd37
akkidg <akashg@zabuzalabs.com>
2016-06-14 14:26:52 +0530

    json string update

--- chat.js
+++ chat.js
@@ -18,7 +18,7 @@ function notifyTyping(){
 }
 
 socket.on('systemMessage',function(jsonText){	
-	//alert('systemMessage '+jsonText.toString());
+	alert('systemMessage '+JSON.stringify(jsonText));
 	var obj = JSON.parse(JSON.stringify(jsonText));	
 	alert('systemMessage '+obj.message);
 	$('#messages').append('<li>' + obj.message + ' </li>');
