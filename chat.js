adb2e426551160b8d107eb02521dd25872692dae
akkidg <akashg@zabuzalabs.com>
2016-06-14 15:46:45 +0530

    test changes3

--- chat.js
+++ chat.js
@@ -5,25 +5,25 @@ function submitfunction(){
 	var msg = $('#m').val();
 	//alert("I am an alert box!");
 	if(msg != ''){
-		socket.emit('chatMessage',user,msg);
+		$('#messages').append('<li><b style="color:green"> Me: </b>' + msg +' </li>');
+		socket.emit('chatMessage',msg);
 	}
 	$('#m').val('').focus();
 	return false;
 }
 
 function notifyTyping(){
-	var user = $('#userName').val();
 	//alert('typing user'+user);
-	socket.emit('typing',username);
+	socket.emit('typing');
 }
 
 socket.on('systemMessage',function(jsonText){	
-	var obj = JSON.parse(jsonText);	
+	var obj = JSON.parse(JSON.stringify(jsonText));	
 	$('#messages').append('<li>' + obj.message + ' </li>');
 });
 
 socket.on('login',function(numUsers){
-	var obj = JSON.parse(numUsers);
+	var obj = JSON.parse(JSON.stringify(numUsers));
 	$('#messages').append('<li>' + obj.numUsers + ' participants.</li>');
 });
 
@@ -36,30 +36,22 @@ socket.on('addUser',function(jsonText){
 });
 
 socket.on('typing',function(jsonText){
-	var me = $('#userName').val();
-	var obj = JSON.parse(jsonText);
-	var user = obj.userName;
-	//alert('me'+ me);
-	if(user != me){
-		$('#notifyUser').text(user + ' is typing...');
-	}
+	var obj = JSON.parse(JSON.stringify(jsonText));
+	var user = obj.username;
+	$('#notifyUser').text(user + ' is typing...');
 	setTimeout(function(){$('#notifyUser').text('');},10000);
 });
 
 socket.on('chatMessage',function(jsonText){
 	var me = $('#userName').val();
-	var obj = JSON.parse(jsonText);
+	var obj = JSON.parse(JSON.stringify(jsonText));
 	var from = obj.username;
 	var msg = obj.message;
-
-	//alert('me'+me);
-	var color = (from == me) ? 'green' : '#009afd';
-	var from = (from == me)	? 'Me' : from;
-	$('#messages').append('<li><b style="color:' + color + '">' + from + '</b>:' + msg + '</li>');	
+	$('#messages').append('<li><b style="color:#009afd">' + from + '</b>:' + msg + '</li>');	
 });
 
 socket.on('userLeft',function(jsonText){
-	var obj = JSON.parse(jsonText);
+	var obj = JSON.parse(JSON.stringify(jsonText));
 	var user = obj.username;
 	var numUsers  = obj.numUsers;
 	$('#messages').append('<li><b style="color:#009afd">' + user + '</b> left room.</li>');	
