bd2e43371a9440a955a3539ed92fdc21984de1af
akkidg <akashg@zabuzalabs.com>
2016-07-05 12:48:11 +0530

    user action updates

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var port = process.env.PORT || 5000
 
 var numUsers = 0;
 var userSocketIds = {};
+var dataJson, title, alert;
 
 var dataKey = 'com.chatapp.data';
 
@@ -34,21 +35,48 @@ io.on('connection',function(socket){
 		//io.emit('systemMessage',"akash");	
 	});
 
-	// Events For Direct Messaging
+	// Event For User status updating (online,typing)
+
+	socket.on('userStatus',function(sender,receiver){
+		int index = userSocketIds[receiver];
+
+		if(index != -1){
+			title = 'status online';
+			alert = {'status':9,'userStatus':1,'sender':sender};
+			dataJson = {'title':title,'alert':alert};
+			io.to(userSocketIds[receiver]).emit('userStatus',dataJson);
+		}			
+	});
+
+	socket.on('typing',function(sender,receiver){
+		int index = userSocketIds[receiver];
+
+		if(index != -1){	
+		title = 'user typing';
+		alert = {'status':10,'sender':sender};
+		dataJson = {'title':title,'alert':alert};			
+		io.to(userSocketIds[receiver]).emit('typing',dataJson);
+		}
+	});	
+
+	socket.on('stopTyping',function(sender,receiver){			
+		int index = userSocketIds[receiver];
+
+		if(index != -1){	
+		title = 'user stop typing';
+		alert = {'status':11,'sender':sender};
+		dataJson = {'title':title,'alert':alert};			
+		io.to(userSocketIds[receiver]).emit('typing',dataJson);
+		}
+	}); 
+
+	// Event For Direct Messaging
 		
 	socket.on('directMessage',function(msg,userId){
 		var msgObj = JSON.parse(msg);		
 		io.to(userSocketIds[userId]).emit('directMessage',msgObj);			
 	});
-	
-	socket.on('typing',function(userId){			
-		io.to(userSocketIds[userId]).emit('typing',{username:socket.username});
-	});	
-
-	socket.on('stopTyping',function(userId){			
-		io.to(userSocketIds[userId]).emit('stopTyping',{username:socket.username});
-	});		
-
+				
 	// Events For Group Management
 
 	/*socket.on('newGroup',function(jsonGroupData){
@@ -65,18 +93,18 @@ io.on('connection',function(socket){
 
 	// Events Group Messaging
 
-	socket.on('groupJoin',function(groupName){
+	/*socket.on('groupJoin',function(groupName){
 		socket.join(groupName);
-	});
+	});*/
 
 	socket.on('groupMessage',function(message,groupName){
 		var msgObj = JSON.parse(message);	
 		socket.broadcast.to(groupName).emit('groupMessage',msgObj);
 	});
 
-	socket.on('groupLeave',function(groupName){
+	/*socket.on('groupLeave',function(groupName){
 		socket.leave(groupName);
-	});
+	});*/
 	
 	socket.on('disconnect',function(){
 		if(addedUser){
